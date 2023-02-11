8d6a3b825a8d566eeea9f4b242c64d471d538d31
EA1HG <ismazg@gmail.com>
2023-02-11 12:49:32 +0100

    Update rtl_test.sh

--- rtl_test.sh
+++ rtl_test.sh
@@ -35,7 +35,7 @@ fi
 
 if pgrep -l dump1090 || pgrep -l readsb
 then
-    echo "dump1090 is still running, can't test the rtl-sdr receiver, please reboot and try again!"
+    echo "dump1090 aún se está ejecutando, no se puede probar el receptor rtl-sdr, reinicie e intente nuevamente!"
     for i in $stop
     do
         systemctl restart $i 2>/dev/null
@@ -47,15 +47,15 @@ fi
 
 echo "-----"
 echo "Las muestras perdidas en los primeros 2 segundos después de comenzar la prueba son comunes y no son un problema!"
-echo "Starting 30 second rtl_test, standby!"
+echo "Prueba de 30 segundos rtl_test, en espera!"
 echo "-----"
 
 timeout 30 rtl_test -s 2400000
 
 echo "-------"
 echo "Prueba terminada!"
-echo "More than 2 lost samples per million or other errors probably mean the receiver isn't working correctly."
-echo "Try another power supply before condemning the receiver though!"
+echo "Más de 2 muestras perdidas por millón u otros errores probablemente significan que el receptor no funciona correctamente."
+echo "Sin embargo, pruebe con otra fuente de alimentación antes de condenar el receptor.!"
 echo "-------"
 
 systemctl restart fr24feed &>/dev/null
@@ -74,15 +74,15 @@ then
     echo "-------"
     dmesg --ctime | grep voltage | tail -n15
     echo "-------"
-    echo "Your power supply is not adequate, consider the Official Raspberry Pi power supply."
-    echo "Any constant voltage supply with 5.1 to 5.2 Volts and 2.5A capability is also a good choice."
+    echo "Su fuente de alimentación no es adecuada, considere la fuente de alimentación de mas potencia."
+    echo "Cualquier suministro de voltaje constante con capacidad de 5,1 a 5,2 voltios y 2,5 A también es una buena opción."
     echo "Las fuentes de alimentación inadecuadas pueden conducir a muchos problemas diferentes!"
     echo "-------"
 else
     echo "-------"
-    echo "No undervoltage detected, looking fine!"
-    echo "If the dongle is not directly plugged into the Raspberry Pi, lack of power/voltage could still be an issue."
-    echo "Even without detected undervoltage a better power supply can often improve reception!"
-    echo "For optimum performance i would recommend the Official Raspberry Pi power supply."
+    echo "No se detectó subtensión, todo ok!"
+    echo "Si el dongle no está conectado directamente a la Raspberry Pi, la falta de energía/voltaje aún podría ser un problema."
+    echo "Incluso sin subtensión detectada, una mejor fuente de alimentación a menudo puede mejorar la recepción!"
+    echo "Para un rendimiento óptimo, recomendaría la fuente de alimentación de calidad y con ptencia minia de 2 amp."
     echo "-------"
 fi
